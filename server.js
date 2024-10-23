ad88f5f4c794ecd083ebbbac848588561ff114ca
jesse-dot <67440987+jesse-dot@users.noreply.github.com>
2024-10-23 14:25:50 -0400

    Add route to serve `index.html` from `public` directory
    * Serve `index.html` file when accessing the root route (`/`)
    * Move existing `index.html` file to the `public` directory

--- server.js
+++ server.js
@@ -86,6 +86,10 @@ app.post('/api/users/:id/channels', (req, res) => {
     }
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
